07c681da5f38a7a4dc7efbbb1b19918e5503bd0b
Cravatec <samuel.wrobel@gmail.com>
2022-02-09 12:38:29 +0100

    Storyboard update

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
 
-
+    @IBOutlet weak var image1: UIImageView!
+    
+    @IBAction func importImage(_ sender: Any) {
+        
+    }
+    
 }
 
